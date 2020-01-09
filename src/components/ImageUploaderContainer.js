85f11ff3f21fdc2b309df1d8691a82ce8f660627
gergohrubo <gergohrubo@gmail.com>
2020-01-09 12:08:03 +0100

    onDrop event handler includes a post request that sends the selected image to the backend via superagent

--- src/components/ImageUploaderContainer.js
+++ src/components/ImageUploaderContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ImageUploader from './ImageUploader'
+import request from 'superagent'
 
 class ImageUploaderContainer extends Component {
   state = {
@@ -7,6 +8,15 @@ class ImageUploaderContainer extends Component {
   }
   onDrop = picture => {
     this.setState({ picture });
+    const data = new FormData()
+    data.append('image', this.state.picture[0], this.state.picture[0].name)
+    request
+      .post(`${process.env.REACT_APP_BACKEND_URL}/image`)
+      .send(data)
+      .then(response => {
+        console.log(response)
+      })
+      .catch(console.error)
   }
   render() {
     return (
